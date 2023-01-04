8b9035a21a46caf1264db9e6e594bcc27654c9da
Ching Pei Yang <badnam3o.0@gmail.com>
2023-01-04 16:39:31 +0100

    fix: fonts

--- modules/wayland/fonts.nix
+++ modules/wayland/fonts.nix
@@ -5,14 +5,15 @@
 }: {
   fonts = {
     fonts = with pkgs; [
-	 	fira
+	 	# fira-code
+	 	# fira-code-symbols
 		lato
       material-icons
       material-design-icons
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
-      (nerdfonts.override {fonts = ["Fira"];})
+      (nerdfonts.override {fonts = ["FiraCode"];})
     ];
 
     enableDefaultFonts = false;
@@ -21,9 +22,9 @@
     fontconfig = {
       defaultFonts = {
         monospace = [
-          #"Iosevka Term"
-          #"Iosevka Term Nerd Font Complete Mono"
-          #"Iosevka Nerd Font"
+          "FiraCode"
+          "FiraCode Term Nerd Font Complete Mono"
+          "FiraCode Nerd Font"
           "Noto Color Emoji"
         ];
         sansSerif = ["Lato" "Noto Color Emoji"];
