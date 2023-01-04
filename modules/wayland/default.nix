269caf6f848599f9c5d62184b943f8c6a4296af8
Ching Pei Yang <badnam3o.0@gmail.com>
2023-01-05 00:49:56 +0100

    feat: add nixpkgs-wayland cachix

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -6,9 +6,16 @@
   imports = [./fonts.nix ];
   # nixpkgs.overlays = with ; [nixpkgs-wayland.overlay];
 
+	# TODO move out
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [
+	 	"https://hyprland.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+	];
+    trusted-public-keys = [
+	 	"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+	];
   };
   
   # Enable sound with pipewire.
