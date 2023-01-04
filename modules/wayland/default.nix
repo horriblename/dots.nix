14eb9d467ed73cd18403c5f107b808445b971124
Ching Pei Yang <badnam3o.0@gmail.com>
2023-01-04 16:41:33 +0100

    feat: add hyprland cache

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -6,6 +6,11 @@
 }: {
   imports = [./fonts.nix ];
   nixpkgs.overlays = with inputs; [nixpkgs-wayland.overlay];
+
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
   
   # Enable sound with pipewire.
   sound.enable = false;
