c2ff4830f0104370090ed75d4e095e9f31afdcb0
Ching Pei Yang <badnam3o.0@gmail.com>
2023-01-04 01:24:29 +0100

    feat: add packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,11 +1,17 @@
 { inputs, pkgs, config, ... }:
 {
 	home.packages = with pkgs; [
+		# cli tools
 		file # lf config dependency
 		bat
 		fzf
 		ripgrep
 		fd
 		lazygit
+
+		# compilers
+		gcc
+		cargo
+		go
 	];
 }
