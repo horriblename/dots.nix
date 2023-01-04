f8afb3f9a517414dfa3571841d1eada08832df0a
Ching Pei Yang <badnam3o.0@gmail.com>
2023-01-04 22:52:19 +0100

    feat: add keyd and config - caps as control/esc

--- hosts/surface/configuration.nix
+++ hosts/surface/configuration.nix
@@ -46,9 +46,31 @@
   };
 
    environment.systemPackages = with pkgs; [
-	git
-     wget
+		git
+     	wget
+		keyd
    ];
+
+	environment.etc."keyd/default.conf".text = ''
+[ids]
+*
+
+[main]
+capslock = overload(control, esc)
+'';
+
+  systemd.services.keyd = {
+    enable = true;
+    description = "key remapping daemon";
+    requires = ["local-fs.target"];
+	 after = ["local-fs.target"];
+    serviceConfig = {
+	 	Type = "simple";
+      ExecStart = "${pkgs.keyd}/bin/keyd";
+    };
+    wantedBy = [ "sysinit.target" ];
+  };
+
 
   programs.neovim = {
   	enable = true;
