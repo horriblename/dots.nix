ef2c1b14628d2dc878533ce445a13975d6ebe92d
Ching Pei Yang <badnam3o.0@gmail.com>
2023-01-16 13:09:26 +0100

    (test) setup distributed builder

--- hosts/surface/configuration.nix
+++ hosts/surface/configuration.nix
@@ -10,7 +10,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "surface"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   time.timeZone = "Europe/Berlin";
 
@@ -24,7 +24,7 @@
 
   users.users.py = {
     isNormalUser = true;
-	 shell = pkgs.zsh;
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" "input" ];
     packages = with pkgs; [
       firefox
@@ -32,27 +32,27 @@
     ];
   };
 
-   environment.systemPackages = with pkgs; [
-		git
-     	wget
-		keyd
-   ];
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    keyd
+  ];
 
-	environment.etc."keyd/default.conf".text = ''
-[ids]
-*
+  environment.etc."keyd/default.conf".text = ''
+    [ids]
+    *
 
-[main]
-capslock = overload(control, esc)
-'';
+    [main]
+    capslock = overload(control, esc)
+  '';
 
   systemd.services.keyd = {
     enable = true;
     description = "key remapping daemon";
-    requires = ["local-fs.target"];
-	 after = ["local-fs.target"];
+    requires = [ "local-fs.target" ];
+    after = [ "local-fs.target" ];
     serviceConfig = {
-	 	Type = "simple";
+      Type = "simple";
       ExecStart = "${pkgs.keyd}/bin/keyd";
     };
     wantedBy = [ "sysinit.target" ];
@@ -60,31 +60,52 @@ capslock = overload(control, esc)
 
 
   programs.neovim = {
-  	enable = true;
-	configure = {
-		customRC = builtins.readFile ../../pkgs/config.vim;
-	};
+    enable = true;
+    configure = {
+      customRC = builtins.readFile ../../pkgs/config.vim;
+    };
   };
 
   services.openssh = {
-  	enable = true;
-	#permitRootLogin = "yes";
+    enable = true;
+    #permitRootLogin = "yes";
   };
 
   nix.settings = {
-  		substituters = [
-			https://cache.nixos.org
-			https://horriblename.cachix.org
-		];
-		trusted-public-keys = [
-			"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-			"horriblename.cachix.org-1:FdI7l8gJJNhehkdW66BGcRrwn+14Iy+oC033gyONcs0="
-		];
+    substituters = [
+      https://cache.nixos.org
+      https://horriblename.cachix.org
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "horriblename.cachix.org-1:FdI7l8gJJNhehkdW66BGcRrwn+14Iy+oC033gyONcs0="
+    ];
+
+    buildMachines = [{
+      hostName = "archbox";
+      system = "x86_64-linux";
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark "
+        big-parallel
+        "kvm"
+      ];
+      mandatoryFeatures = [ ];
+    }];
+    distributedBuilds = true;
+    extraOptions = ''
+      			builders-use-substitutes = true
+      		'';
   };
 
   # system.copySystemConfiguration = true;
 
-  system.stateVersion = "22.11"; # DO NOT CHANGE THIS
+  system.stateVersion = "
+        22.11
+        "; # DO NOT CHANGE THIS
 
 }
+
 
