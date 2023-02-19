d97e688af56830b554f42875585d3cac8a1c4f09
Ching Pei Yang <badnam3o.0@gmail.com>
2023-02-20 00:11:40 +0100

    fix: tofi font

--- modules/home/tofi/default.nix
+++ modules/home/tofi/default.nix
@@ -2,7 +2,28 @@
 {
   home.packages = with pkgs; [
     tofi
+    lato # font
   ];
 
-  xdg.configFile."tofi/gruvbox-menu.ini".text = builtins.readFile ./gruvbox-menu.ini;
+  xdg.configFile."tofi/gruvbox-menu.ini".text = ''
+    font = ${pkgs.lato}/share/fonts/lato/Lato-Regular.ttf
+    font-size = 16
+    corner-radius = 30
+    outline-color = #FE8019
+    outline-width = 1
+    border-color = #FE8019
+    border-width = 1
+    background-color = #282828
+    text-color = #A2BAB1
+    #selection-color = #B8BB26
+    selection-color = #FB4924
+    selection-match-color = #8EC07C
+    #prompt-text = "  "
+    prompt-text = "> "
+    #num-results = 9
+    hide-cursor = true
+    width = 30%
+    height = 50%
+    drun-launch = true
+  '';
 }
