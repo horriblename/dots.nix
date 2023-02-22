90c21e083349a35bdc73ec33d41bf6bbc41177bc
Ching Pei Yang <badnam3o.0@gmail.com>
2023-02-22 02:55:41 +0100

    feat(foot): fixed cursor color

--- modules/home/foot/default.nix
+++ modules/home/foot/default.nix
@@ -14,6 +14,9 @@
       colors = {
         alpha = "0.6";
       };
+      cursor = {
+        color = "282828 ebdbb2"; 
+      };
       key-bindings = {
         search-start = "Control+Shift+f";
       };
