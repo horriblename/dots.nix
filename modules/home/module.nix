aca478ede48ac305d9a50916f098ff90ee0f0ac3
Ching Pei Yang <badnam3o.0@gmail.com>
2023-04-23 13:31:33 +0200

    add module option homeConfigurationMode

--- modules/home/module.nix
+++ modules/home/module.nix
@@ -1,5 +1,17 @@
 {lib, ...}:
 with lib; {
+  options.homeConfigurationMode = mkOption {
+    type = types.enum [
+      "cli-tools"
+      "graphical"
+    ];
+    description = ''
+      What level of configuration do you want with home-manager?
+      "cli-tools" gives a fully-fledged cli environment
+      "graphical" also installs GUI apps + hyprland
+    '';
+  };
+
   options.machineName = mkOption {
     type = types.enum [
       "archbox"
