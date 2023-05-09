d0964b0313c46a7d1a53b187caa2117abf953ab3
Ching Pei Yang <badnam3o.0@gmail.com>
2023-05-09 12:33:45 +0200

    (nvim)extra keybinds

--- modules/home/terminal/nvim/default.nix
+++ modules/home/terminal/nvim/default.nix
@@ -308,6 +308,26 @@
         "<leader>gdd".action = ":DiffviewOpen ";
         "<leader>gdm".action = ":DiffviewOpen<CR>";
         "<leader>gdh".action = ":DiffviewFileHistory %<CR>";
+
+        # Git
+        "<leader>gu".action = "<cmd>Gitsigns undo_stage_hunk<CR>";
+        "<leader>g<C-w>".action = "<cmd>Gitsigns preview_hunk<CR>";
+        "<leader>gp".action = "<cmd>Gitsigns prev_hunk<CR>";
+        "<leader>gn".action = "<cmd>Gitsigns next_hunk<CR>";
+        "<leader>gP".action = "<cmd>Gitsigns preview_hunk_inline<CR>";
+        "<leader>gR".action = "<cmd>Gitsigns reset_buffer<CR>";
+        "<leader>gb".action = "<cmd>Gitsigns blame_line<CR>";
+        "<leader>gD".action = "<cmd>Gitsigns diffthis HEAD<CR>";
+        "<leader>gw".action = "<cmd>Gitsigns toggle_word_diff<CR>";
+
+        # Telescope
+        "<M-f>".action = ":Telescope resume<CR>";
+        "<leader>fq".action = ":Telescope quickfix<CR>";
+      };
+
+      vim.maps.normalVisualOp = {
+        "<leader>gs".action = ":Gitsigns stage_hunk<CR>";
+        "<leader>gr".action = ":Gitsigns reset_hunk<CR>";
       };
 
       vim.maps.insert = {
