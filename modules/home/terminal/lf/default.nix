bac9776149893894e08792243e772f373384c870
Ching Pei Yang <badnam3o.0@gmail.com>
2023-05-21 22:05:08 +0200

    use upstream lf

--- modules/home/terminal/lf/default.nix
+++ modules/home/terminal/lf/default.nix
@@ -41,15 +41,6 @@ in {
   ];
   programs.lf = {
     enable = true;
-    package = pkgs.lf.overrideAttrs (prevAttrs: {
-      version = lf-version;
-      src = pkgs.fetchFromGitHub {
-        owner = "horriblename";
-        repo = "lf";
-        rev = "r${lf-version}";
-        hash = "sha256-VEXWjpdUP5Kabimp9kKoLR7/FlE39MAroRBl9au2TI8=";
-      };
-    });
     extraConfig = builtins.readFile ./lfrc;
     previewer.source = ./preview;
   };
