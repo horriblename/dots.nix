a97a40d7e5479266ddcda19b0671664ed40eb734
Ching Pei Yang <badnam3o.0@gmail.com>
2023-05-24 12:24:35 +0200

    (nvim) disable some keybinds

--- modules/home/terminal/nvim/default.nix
+++ modules/home/terminal/nvim/default.nix
@@ -282,8 +282,8 @@
       setup = ''
         require('aerial').setup({
           on_attach = function(bufnr)
-            vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-            vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
+            -- vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
+            -- vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
           end
         })
       '';
