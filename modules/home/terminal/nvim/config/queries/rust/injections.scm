2127464769763a42e0a1d50a1975d5afd4b2f515
Ching Pei Yang <badnam3o.0@gmail.com>
2023-11-21 02:06:27 +0100

    fix(nvim): outdated query

--- modules/home/terminal/nvim/config/queries/rust/injections.scm
+++ modules/home/terminal/nvim/config/queries/rust/injections.scm
@@ -1,5 +1,4 @@
 ; extends
 ((line_comment) @markdown_inline
 					 (#match? @markdown_inline "^///")
-					 (#offset! @markdown_inline 0 3 0 0)
-					 (#exclude_children! @markdown_inline))
+					 (#offset! @markdown_inline 0 3 0 0))
