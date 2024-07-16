ec4cae22527a722daaa56b8dd8198fceb0155cdc
Pei Yang Ching <59727193+horriblename@users.noreply.github.com>
2024-07-16 20:10:28 +0200

    nvim: split devicon_conceal into plugin/

---
+++ modules/home/terminal/nvim/config/plugin/devicon_conceal.lua
@@ -0,0 +1,12 @@
+local function conceal_as_devicon(match, _, bufnr, pred, metadata)
+	if #pred == 2 then
+		-- (#as_devicon! @capture)
+		local capture_id = pred[2]
+		local lang = vim.treesitter.get_node_text(match[capture_id], bufnr)
+
+		local icon, _ = require("nvim-web-devicons").get_icon_by_filetype(lang, { default = true })
+		metadata["conceal"] = icon
+	end
+end
+
+vim.treesitter.query.add_directive("as_devicon!", conceal_as_devicon, true)
