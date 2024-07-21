9d0003d7ab89789ac183ec21cac84068ed61b7a2
Pei Yang Ching <59727193+horriblename@users.noreply.github.com>
2024-07-21 14:07:03 +0200

    lint: remove unused

--- modules/home/terminal/nvim/default.nix
+++ modules/home/terminal/nvim/default.nix
@@ -1,19 +1,8 @@
 {
   self,
-  lib,
-  config,
   inputs,
-  pkgs,
-  impurity,
   ...
-} @ args: let
-  nix2Lua = import ./lib/toLua.nix;
-  rawLua = str: {
-    _type = "rawLua";
-    lua = str;
-  };
-  setup = module: table: "require('${module}').setup ${nix2Lua table}";
-in {
+} @ args: {
   imports = [
     inputs.nvf.homeManagerModules.default
     inputs.impurity.nixosModules.default
