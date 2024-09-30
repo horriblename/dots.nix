97528664f2b2b914b171f5f62cbb1d4f7f698457
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-09-30 15:17:56 +0200

    flake: update hyprland input url

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "github.com/hyprwm/Hyprland";
     hyprgrass = {
       url = "github:horriblename/hyprgrass";
       inputs.hyprland.follows = "hyprland";
