3f0db37a68690121dcd6a05a99ee50cadd649318
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-05 20:11:36 +0200

    home: add nix-path

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,6 +1,12 @@
-{lib, ...}: {
+{
+  lib,
+  inputs,
+  self,
+  ...
+}: {
   home.username = lib.mkDefault "py";
   home.homeDirectory = lib.mkDefault "/home/py";
+  nix.settings.nix-path = ["nixpkgs=${inputs.nixpkgs}" "dots=${self}"];
   xdg.enable = true;
   imports = [
     ./module.nix
