b3b325272a424f5c3da698e03153f26a9952052f
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-08 00:18:54 +0200

    neo-tree: disable

--- modules/home/terminal/nvim/nvf.nix
+++ modules/home/terminal/nvim/nvf.nix
@@ -278,7 +278,7 @@ in {
 
   vim.filetree = {
     neo-tree = {
-      enable = true;
+      enable = false;
       setupOpts = {
         git_status_async = true;
         window = {
@@ -473,7 +473,7 @@ in {
   vim.maps.normal = {
     # General
     "<leader>zf".action = ":lua vim.g.formatsave = not vim.g.formatsave<CR>";
-    "<leader>e".action = ":Neotree toggle reveal<CR>";
+    # "<leader>e".action = ":Neotree toggle reveal<CR>";
     "<leader>ld".action = ":lua vim.diagnostic.setqflist({open = true})<CR>";
     "<leader>lf".action = ":lua vim.lsp.buf.format()<CR>";
     "<leader>li".action = ":lua vim.lsp.buf.implementation()<CR>";
