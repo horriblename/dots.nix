ae21fb6ffa675cea38d7ba98aa02e6c64460a2ac
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-07 14:22:32 +0200

    home: include optional local nix.cond

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -7,6 +7,7 @@
   home.username = lib.mkDefault "py";
   home.homeDirectory = lib.mkDefault "/home/py";
   nix.settings.nix-path = ["nixpkgs=${inputs.nixpkgs}" "dots=${self}"];
+  nix.extraOptions = "!include nix.local.conf";
   xdg.enable = true;
   imports = [
     ./module.nix
