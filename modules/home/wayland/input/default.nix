74ebc8bdda667c6e5bead85c82228c6692ab1115
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-28 00:45:30 +0100

    home/fcitx5: re-enable

--- modules/home/wayland/input/default.nix
+++ modules/home/wayland/input/default.nix
@@ -5,12 +5,12 @@
   ...
 }: {
   config = lib.mkIf config.dots.wayland.enable {
-    # i18n.inputMethod = {
-    #   enabled = "fcitx5";
-    #   # fcitx5.addons = with pkgs; [
-    #   #   fcitx5-gtk
-    #   #   fcitx5-qt
-    #   # ];
-    # };
+    i18n.inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-chinese-addons
+        fcitx5-configtool
+      ];
+    };
   };
 }
