3a367a7431140bd2e483f26b3e61d89353946713
Ching Pei Yang <badnam3o.0@gmail.com>
2024-11-01 01:36:02 +0100

    nixos/login: switch to lightdm

--- modules/nixos/desktop/login.nix
+++ modules/nixos/desktop/login.nix
@@ -1,16 +1,25 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   inherit (lib.modules) mkIf;
 in {
   config = mkIf config.dots.wayland.enable {
-    services.greetd = {
+    environment.systemPackages = [pkgs.onboard];
+    services.xserver = {
       enable = true;
-    };
-    programs.regreet = {
-      enable = true;
+      displayManager.lightdm = {
+        enable = true;
+        greeters.gtk = {
+          enable = true;
+          indicators = ["~language" "~a11y" "~session" "~clock" "~power"];
+          extraConfig = ''
+            keyboard=onboard
+          '';
+        };
+      };
     };
   };
 }
