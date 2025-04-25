0620779c1fe5d28069cd5c0c88657f5b6d9d0780
Ching Pei Yang <badnam3o.0@gmail.com>
2025-04-25 18:21:38 +0200

    home/apps: add onlyoffice

--- modules/home/apps/default.nix
+++ modules/home/apps/default.nix
@@ -11,6 +11,18 @@ in {
       firefox
       kdePackages.okular
       image-roll
+      onlyoffice-bin
+      # NOTE: manual intervention needed:
+      # copy corefonts into ~/.local/share/fonts/truetype/*.ttf
+      # (symlinks don't work due to bug)
+      # corefonts
     ];
+
+    nixpkgs.config = {
+      allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [
+          "corefonts"
+        ];
+    };
   };
 }
