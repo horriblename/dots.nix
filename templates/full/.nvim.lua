bfba04015a9e1c87a9bc90c045ffb25eb1d7c864
Ching Pei Yang <badnam3o.0@gmail.com>
2025-05-16 13:24:48 +0200

    templates/full: update .nvim.lua

--- templates/full/.nvim.lua
+++ templates/full/.nvim.lua
@@ -1,25 +1,20 @@
-local lspconfig = require('lspconfig')
-
-local old = lspconfig.nixd
-
-lspconfig.nixd.setup({
-    cmd = old.cmd,
-    settings = {
-        nixd = {
-            nixpkgs = {
-                expr = 'import (builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs {}',
-            },
-            formatting = {
-                command = { "alejandra" },
-            },
-            options = {
-                nixos = {
-                    expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.ragnarok.options',
-                },
-                home_manager = {
-                    expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."py@ragnarok".options'
-                },
-            },
-        },
-    },
+vim.lsp.config("nixd", {
+	settings = {
+		nixd = {
+			nixpkgs = {
+				expr = 'import (builtins.getFlake ("git+file://" + toString ./.)).inputs.nixpkgs {}',
+			},
+			formatting = {
+				command = { "alejandra" },
+			},
+			options = {
+				nixos = {
+					expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.ragnarok.options',
+				},
+				home_manager = {
+					expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."py@ragnarok".options',
+				},
+			},
+		},
+	},
 })
