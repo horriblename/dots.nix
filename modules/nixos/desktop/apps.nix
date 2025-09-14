2568c80501f03622cec5b7267d9a7d30b2723e86
Ching Pei Yang <badnam3o.0@gmail.com>
2025-09-14 11:38:23 +0200

    nixos/desktop: add obs + virtual cam

--- modules/nixos/desktop/apps.nix
+++ modules/nixos/desktop/apps.nix
@@ -1,3 +1,22 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
   services.gvfs.enable = true;
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      droidcam-obs
+    ];
+  };
 }
