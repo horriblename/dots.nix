703a871b8dcf1ed846d659e036cc56e81210c379
Ching Pei Yang <badnam3o.0@gmail.com>
2025-12-03 02:12:45 +0100

    nvim: extract autocmd callback into own function

--- modules/home/terminal/nvim/config/plugin/fixwfh.lua
+++ modules/home/terminal/nvim/config/plugin/fixwfh.lua
@@ -19,9 +19,6 @@ local function find_parent_layout(target, layout)
 	return nil, 0
 end
 
-local function debug_layout(node)
-end
-
 ---checks if any nested child sticking to the specified side has the
 ---given option enabled
 ---@param opt string
@@ -72,87 +69,94 @@ end
 ---@type table<integer, {direction: direction, size: integer}>
 local needs_resize = {}
 
+local function on_win_closed(ev)
+	local closed_winid = tonumber(ev.match)
+	vim.notify("[fixwfh] closed win: " .. closed_winid, vim.log.levels.TRACE)
+	if not closed_winid then
+		vim.notify("[fixwfh] invalid closed_winid", vim.log.levels.TRACE)
+		return
+	end
+
+	local tabnr = vim.fn.win_id2tabwin(closed_winid)[1]
+	if tabnr == 0 then
+		vim.notify("[fixwfh] tab does not exist", vim.log.levels.TRACE)
+		return
+	end
+
+	local layout = vim.fn.winlayout(tabnr)
+	if #layout == 0 or layout[1] == "leaf" then
+		vim.notify("[fixwfh] top-level is a leaf", vim.log.levels.TRACE)
+		return
+	end
+
+	---@cast layout vim.fn.winlayout.branch
+
+	---@type direction
+	local direction
+	local sibling
+	local side ---@type side
+
+	do
+		local parent, closed_index = find_parent_layout(closed_winid, layout)
+		if parent == nil then
+			vim.notify("[fixwfh] closed winid not found in layout", vim.log.levels.TRACE)
+			return
+		end
+
+		assert(parent[2][closed_index][2] == closed_winid)
+		if parent[2][closed_index - 1] then
+			sibling = parent[2][closed_index - 1]
+			side = parent[1] == "row" and "right" or "down"
+		elseif parent[2][closed_index + 1] then
+			sibling = parent[2][closed_index + 1]
+			side = parent[1] == "row" and "left" or "up"
+		else
+			vim.notify("[fixwfh] no sibling found", vim.log.levels.TRACE)
+			return
+		end
+
+		direction = parent[1] == "row" and "horizontal" or "vertical"
+	end
+
+	-- look for windows in the sibling row/column with winfixheight/width
+	local option = direction == "vertical" and "winfixheight" or "winfixwidth"
+	local descendants ---@type integer[]
+	if sibling[1] == "leaf" then
+		vim.notify("[fixwfh] sibling is leaf", vim.log.levels.TRACE)
+		return
+	else
+		local winids = leaves_with_option_enabled(option, sibling, side)
+		if #winids == 0 then
+			vim.notify("[fixwfh] no descendant of sibling has " .. option, vim.log.levels.TRACE)
+			return
+		end
+		descendants = winids
+	end
+
+	for _, node in ipairs(descendants) do
+		local size = direction == "vertical"
+			and vim.api.nvim_win_get_height(node)
+			or vim.api.nvim_win_get_width(node)
+
+		-- schedule a resize after window is removed from layout
+		needs_resize[node] = {
+			direction = direction,
+			size = size,
+		}
+	end
+
+	vim.notify("[fixwfh] scheduling resize" .. vim.inspect(needs_resize), vim.log.levels.TRACE)
+end
+
 local group = vim.api.nvim_create_augroup("fix_winfixheight", { clear = true })
 vim.api.nvim_create_autocmd("WinClosed", {
 	group = group,
-	callback = function(ev)
-		local closed_winid = tonumber(ev.match)
-		vim.notify("[fixwfh] closed win: " .. closed_winid, vim.log.levels.TRACE)
-		if not closed_winid then
-			vim.notify("[fixwfh] invalid closed_winid", vim.log.levels.TRACE)
-			return
+	callback = function (ev)
+		local ok, err = pcall(on_win_closed, ev)
+		if not ok then
+			vim.notify_once(err, vim.log.levels.ERROR)
 		end
-
-		local tabnr = vim.fn.win_id2tabwin(closed_winid)[1]
-		if tabnr == 0 then
-			vim.notify("[fixwfh] tab does not exist", vim.log.levels.TRACE)
-			return
-		end
-
-		local layout = vim.fn.winlayout(tabnr)
-		if #layout == 0 or layout[1] == "leaf" then
-			vim.notify("[fixwfh] top-level is a leaf", vim.log.levels.TRACE)
-			return
-		end
-
-		---@cast layout vim.fn.winlayout.branch
-
-		---@type direction
-		local direction
-		local sibling
-		local side ---@type side
-
-		do
-			local parent, closed_index = find_parent_layout(closed_winid, layout)
-			if parent == nil then
-				vim.notify("[fixwfh] closed winid not found in layout", vim.log.levels.TRACE)
-				return
-			end
-
-			assert(parent[2][closed_index][2] == closed_winid)
-			if parent[2][closed_index - 1] then
-				sibling = parent[2][closed_index - 1]
-				side = parent[1] == "row" and "right" or "down"
-			elseif parent[2][closed_index + 1] then
-				sibling = parent[2][closed_index + 1]
-				side = parent[1] == "row" and "left" or "up"
-			else
-				vim.notify("[fixwfh] no sibling found", vim.log.levels.TRACE)
-				return
-			end
-
-			direction = parent[1] == "row" and "horizontal" or "vertical"
-		end
-
-		-- look for windows in the sibling row/column with winfixheight/width
-		local option = direction == "vertical" and "winfixheight" or "winfixwidth"
-		local descendants ---@type integer[]
-		if sibling[1] == "leaf" then
-			vim.notify("[fixwfh] sibling is leaf", vim.log.levels.TRACE)
-			return
-		else
-			local winids = leaves_with_option_enabled(option, sibling, side)
-			if #winids == 0 then
-				vim.notify("[fixwfh] no descendant of sibling has " .. option, vim.log.levels.TRACE)
-				return
-			end
-			descendants = winids
-		end
-
-		for _, node in ipairs(descendants) do
-			local size = direction == "vertical"
-				and vim.api.nvim_win_get_height(node)
-				or vim.api.nvim_win_get_width(node)
-
-			-- schedule a resize after window is removed from layout
-			needs_resize[node] = {
-				direction = direction,
-				size = size,
-			}
-		end
-
-		vim.notify("[fixwfh] scheduling resize" .. vim.inspect(needs_resize), vim.log.levels.TRACE)
-	end
+	end,
 })
 
 vim.api.nvim_create_autocmd("WinResized", {
