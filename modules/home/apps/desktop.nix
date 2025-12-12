52f6c55336c8341b522be35505be478e61ca4c79
Ching Pei Yang <badnam3o.0@gmail.com>
2025-12-12 22:45:02 +0100

    ollama: tweaks

--- modules/home/apps/desktop.nix
+++ modules/home/apps/desktop.nix
@@ -8,6 +8,18 @@ in {
   config = mkIf (config.dots.preset == "archbox") {
     services.ollama = {
       enable = true;
+      environmentVariables = {
+        OLLAMA_CONTEXT_LENGTH = "16000";
+      };
+    };
+    systemd.user.services = {
+      ollama = {
+        Service = {
+          Nice = 19;
+          IOSchedulingClass = "best-effort";
+          IOSchedulingPriority = 7;
+        };
+      };
     };
   };
 }
