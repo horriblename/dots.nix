48f64879ced85390821c95d24ba8d392d032d521
Ching Pei Yang <badnam3o.0@gmail.com>
2026-01-26 13:16:01 +0100

    nixos: add ai option

--- modules/nixos/desktop/apps/open-webui.nix
+++ modules/nixos/desktop/apps/open-webui.nix
@@ -5,9 +5,10 @@
 }: let
   inherit (lib.modules) mkIf;
 in
-  mkIf (config.dots.preset == "archbox") {
+  mkIf config.dots.ai.enable {
     services.open-webui = {
       enable = true;
+      port = 11435;
       environment = {
         OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
         # Disable authentication
