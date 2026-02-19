f06f52e70d0a709e66da0e7169971abeb53a7f13
Ching Pei Yang <badnam3o.0@gmail.com>
2026-02-19 16:03:13 +0100

    nvim/diagnostic: show source in float

--- modules/home/terminal/nvim/config/plugin/nvim.lua
+++ modules/home/terminal/nvim/config/plugin/nvim.lua
@@ -10,3 +10,12 @@ local function diagnosticJump(dir)
 end
 vim.keymap.set("n", "[d", diagnosticJump(-1))
 vim.keymap.set("n", "]d", diagnosticJump(1))
+
+vim.diagnostic.config({
+	float = {
+		suffix = function(diag)
+			return string.format(" [%s by %s]", diag.code, diag.source)
+		end
+	},
+	severity_sort = true,
+})
