8e23119dcee7c91edb9fe44eea5d5d4ad50271e9
Ching Pei Yang <badnam3o.0@gmail.com>
2026-03-13 20:41:11 +0100

    nixos/wakapi: fix

--- modules/nixos/desktop/apps/wakapi.nix
+++ modules/nixos/desktop/apps/wakapi.nix
@@ -5,9 +5,13 @@
 }: let
   inherit (lib.modules) mkIf;
 in
-  mkIf config.dots.development {
+  mkIf config.dots.development.enable {
     services.wakapi = {
       enable = true;
       database.dialect = "sqlite3";
+      passwordSaltFile = "/etc/wakapi/salt";
+      settings = {
+        server.port = 3474;
+      };
     };
   }
