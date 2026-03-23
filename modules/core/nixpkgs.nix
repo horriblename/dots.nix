555d8242391a36d1ff3378dd41496b91e9d3f2cb
Ching Pei Yang <badnam3o.0@gmail.com>
2026-03-23 12:25:17 +0100

    core: split out nixpkgs config

--- modules/core/nixpkgs.nix
+++ modules/core/nixpkgs.nix
@@ -1,38 +1,10 @@
 {
-  lib,
   self,
+  lib,
   ...
-}: let
-  inherit (builtins) elem;
-  inherit (lib.strings) hasPrefix getName;
-in {
+}: {
   nixpkgs = {
     overlays = [self.overlay];
-    config = {
-      allowAliases = false;
-      allowUnfreePredicate = pkg:
-        elem (getName pkg) [
-          "codeium"
-          "unityhub"
-          "open-webui"
-          "steam"
-          "steam-original"
-          "steam-unwrapped"
-          "steam-run"
-          "corefonts"
-
-          "nvidia-x11"
-          "nvidia-settings"
-          "nvidia-persistenced"
-
-          # nvtop-nvidia BS
-          "nvidia"
-          "cuda-merged"
-          "libnvjitlink"
-          "libnpp"
-        ]
-        || hasPrefix "cuda_" (getName pkg)
-        || hasPrefix "libcu" (getName pkg);
-    };
+    config = import ./nixpkgs/config.nix {inherit lib;};
   };
 }
