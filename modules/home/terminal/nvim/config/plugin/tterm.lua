101893e8f70a705af6477f215e630b40a0fe071f
Ching Pei Yang <badnam3o.0@gmail.com>
2026-05-17 00:44:38 +0200

    nvim/tterm: close all floating on unfocus

--- modules/home/terminal/nvim/config/plugin/tterm.lua
+++ modules/home/terminal/nvim/config/plugin/tterm.lua
@@ -3,15 +3,17 @@ vim.api.nvim_create_autocmd("TabClosed", {
 	group = group,
 	callback = function() require("tterm").on_tab_closed() end
 })
-vim.api.nvim_create_autocmd("WinEnter", {
+vim.api.nvim_create_autocmd("WinLeave", {
 	group = group,
-	callback = vim.schedule_wrap(function()
-		if not vim.w.toggleterm_win_offset
-			and vim.api.nvim_win_get_config(0).relative == ''
-		then
-			require("tterm").close_floating_wins(vim.api.nvim_win_get_tabpage(0))
+	callback = function()
+		if vim.w.toggleterm_win_offset then
+			vim.schedule(function()
+				if vim.api.nvim_win_get_config(0).relative == '' then
+					vim.cmd("fclose!")
+				end
+			end)
 		end
-	end),
+	end,
 })
 
 local opts = { remap = false }
