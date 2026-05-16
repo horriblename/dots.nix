49e378dc0c4df53670bec1311f5fef1c28c90627
Ching Pei Yang <badnam3o.0@gmail.com>
2026-05-17 00:56:26 +0200

    nixos/obs: fix obs installed unconditionally

--- modules/nixos/desktop/apps/obs.nix
+++ modules/nixos/desktop/apps/obs.nix
@@ -1,22 +1,27 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
-  services.gvfs.enable = true;
+}: let
+  inherit (lib.modules) mkIf;
+in {
+  config = mkIf config.dots.wayland.enable {
+    services.gvfs.enable = true;
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
-
-  programs.obs-studio = {
-    enable = true;
-    enableVirtualCamera = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      droidcam-obs
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
     ];
+    boot.extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
+
+    programs.obs-studio = {
+      enable = true;
+      enableVirtualCamera = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        droidcam-obs
+      ];
+    };
   };
 }
