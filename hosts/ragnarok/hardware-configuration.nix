320adfe442ebd042e0a3e4d88a1ff3880526bbf4
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-03 20:28:14 +0200

    host/ragnarok: fix breakage

--- hosts/ragnarok/hardware-configuration.nix
+++ hosts/ragnarok/hardware-configuration.nix
@@ -69,7 +69,7 @@
 
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
     };
   };
 
