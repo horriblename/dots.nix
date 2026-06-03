174c12ebf5b6f1de81512b396c30d5cdc95b85c1
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-03 20:06:55 +0200

    home: switch EDITOR to nvf

--- modules/home/terminal/shell/default.nix
+++ modules/home/terminal/shell/default.nix
@@ -10,7 +10,7 @@
   home = {
     sessionVariables = {
       # XDG_DATA_DIRS = "${config.home.profileDirectory}/share\${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}";
-      EDITOR = lib.getExe pkgs.neovim;
+      EDITOR = lib.getExe config.programs.nvf.finalPackage;
       PAGER = "less -FR";
       CARGO_HOME = "${config.xdg.dataHome}/cargo";
       RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
