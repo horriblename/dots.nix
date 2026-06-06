3c089e5bb777c8671941227218397fcfbaf02f5c
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-07 01:15:24 +0200

    host/ragnarok: llama-cpp tweaks

--- hosts/ragnarok/llama.nix
+++ hosts/ragnarok/llama.nix
@@ -3,24 +3,21 @@
   self,
   ...
 }: let
-  inherit (builtins) concatStringsSep;
+  inherit (builtins) concatStringsSep mapAttrs attrValues;
 
   llama-server-git = "${self.packages.${pkgs.stdenv.system}.llama-cpp}/bin/llama-server";
   llama-server = "${pkgs.llama-cpp}/bin/llama-server";
   qwen3-coder-model = "/home/py/.ollama/models/blobs/sha256-1194192cf2a187eb02722edcc3f77b11d21f537048ce04b67ccf8ba78863006a";
 
   llamaSwapConfigFile = pkgs.writers.writeYAML "config.yml" llamaSwapConfig;
-  buildServerCmd = flags:
-    concatStringsSep " " ([
-        "${llama-server-git}"
-        "--ctx-size 16000"
-        "--n-gpu-layers 0"
-        "--jinja"
-      ]
-      ++ flags);
-
-  qwen3CoderServerCmd = port:
-    buildServerCmd ["-m ${qwen3-coder-model}" "--port ${port}"];
+  buildServerCmd' = cmd: flags:
+    "${cmd} "
+    + concatStringsSep " " (
+      attrValues
+      (mapAttrs
+        (key: val: "--${key} ${toString val}")
+        flags)
+    );
 
   # ttl is in seconds
   mkModel = opt: {ttl = 30 * 60;} // opt;
@@ -28,40 +25,42 @@
   llamaSwapConfig = {
     models = {
       "qwen3-coder:30b" = mkModel {
-        cmd = qwen3CoderServerCmd "\${PORT}";
+        cmd = buildServerCmd' "${llama-server-git}" {
+          model = qwen3-coder-model;
+          port = "\${PORT}";
+        };
         aliases = ["qwen3-coder"];
       };
 
       "qwen3-coder-next" = mkModel {
-        cmd = buildServerCmd [
-          "-hf unsloth/Qwen3-Coder-Next-GGUF:UD-Q2_K_XL"
-          "--port \${PORT}"
-          "--threads 15"
-        ];
+        cmd = buildServerCmd' "${llama-server-git}" {
+          hf-repo = "unsloth/Qwen3-Coder-Next-GGUF:UD-Q2_K_XL";
+          ctx-size = 32000;
+          n-gpu-layers = 0;
+          port = "\${PORT}";
+          threads = 15;
+        };
       };
 
       "qwen2.5-coder-3b" = mkModel {
-        cmd = concatStringsSep " " [
-          "${llama-server-git}"
-          "-hf ggml-org/Qwen2.5-Coder-3B-Q8_0-GGUF"
-          "--port \${PORT}"
-          "--threads 8"
-          "--n-gpu-layers 0"
-          "--ctx-size 4096"
-          "--temp 0.1"
-        ];
+        cmd = buildServerCmd' "${llama-server-git}" {
+          port = "\${PORT}";
+          ctx-size = 4096;
+          n-gpu-layers = 0;
+          threads = 8;
+          temp = "0.1";
+        };
       };
 
       "DeepSeek-Coder-V2-Lite-Base" = mkModel {
-        cmd = concatStringsSep " " [
-          "${llama-server}"
-          "-hf QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF"
-          "--port \${PORT}"
-          "--n-gpu-layers 0"
-          "--threads 8"
-          "--ctx-size 512"
-          "--temp 0.1"
-        ];
+        cmd = buildServerCmd' "${llama-server}" {
+          hf-repo = "QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF";
+          n-gpu-layers = 0;
+          port = "\${PORT}";
+          threads = 8;
+          ctx-size = 512;
+          temp = 0.1;
+        };
         aliases = ["DeepSeek-Coder-V2"];
       };
     };
