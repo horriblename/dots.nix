c80d14985f07be23dfb3c8ed974a6bd3416a48b4
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-11 12:29:05 +0200

    core: cleanup broken ref

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -7,27 +7,27 @@
 }: let
   inherit (builtins) mapAttrs;
   inherit (lib.attrsets) filterAttrs;
+
   flakeInputs = filterAttrs (_: input: input.flake or true) inputs;
   flakeRegistries = mapAttrs (_: input: {flake = input;}) flakeInputs;
-in {
-  nix.package = pkgs.nix;
-  nix.extraOptions = "experimental-features = nix-command flakes";
-  nix.registry =
-    {
-      n.flake = inputs.nixpkgs;
-      nixpkgs.flake = inputs.nixpkgs;
-      nixgl.flake = inputs.nixgl;
-      dots.flake = self;
-      roc.flake = inputs.roc;
-    }
-    // flakeRegistries;
 
-  # TODO move out
-  nix.settings = let
-    inherit ((import ../../flake.nix).nixConfig) extra-substituters extra-trusted-public-keys;
-  in {
-    # nix-path = ["nixpkgs=${inputs.nixpkgs}" "dots=${self}"];
-    substituters = extra-substituters;
-    trusted-public-keys = extra-trusted-public-keys;
+  inherit ((import ../../flake.nix).nixConfig) extra-substituters extra-trusted-public-keys;
+in {
+  nix = {
+    package = pkgs.nix;
+    extraOptions = "experimental-features = nix-command flakes";
+    registry =
+      flakeRegistries
+      // {
+        n.flake = inputs.nixpkgs;
+        nixpkgs.flake = inputs.nixpkgs;
+        nixgl.flake = inputs.nixgl;
+        dots.flake = self;
+      };
+    settings = {
+      # nix-path = ["nixpkgs=${inputs.nixpkgs}" "dots=${self}"];
+      substituters = extra-substituters;
+      trusted-public-keys = extra-trusted-public-keys;
+    };
   };
 }
