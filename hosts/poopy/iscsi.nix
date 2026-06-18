954ca3a37f7accd2515d4c324062d3fdf13a39dd
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-18 23:32:00 +0200

    hosts/poopy: document iscsi steps

--- hosts/poopy/iscsi.nix
+++ hosts/poopy/iscsi.nix
@@ -1,4 +1,12 @@
 # block storage mounts
+#
+# XXX: Manual steps:
+#
+#   sudo iscsiadm -m node -o new -T iqn.2015-12.com.oracleiaas:6a2e8816-5763-4768-8fbf-1cc9070042d2 -p 169.254.2.2:3260
+#   sudo iscsiadm -m node -o update -T iqn.2015-12.com.oracleiaas:6a2e8816-5763-4768-8fbf-1cc9070042d2 -n node.startup -v automatic
+#   sudo iscsiadm -m node -T iqn.2015-12.com.oracleiaas:6a2e8816-5763-4768-8fbf-1cc9070042d2 -p 169.254.2.2:3260 -l
+#
+# It should be possible to wrap this script into a systemd unit but I'm too lazy
 {
   services = {
     openiscsi = {
@@ -6,11 +14,5 @@
       discoverPortal = "169.254.2.2:3260";
       name = "iqn.2015-12.com.oracleiaas:6a2e8816-5763-4768-8fbf-1cc9070042d2";
     };
-
-    # fileSystems."/state" = {
-    #   device = "/dev/disk/by-uuid/6a2e8816-5763-4768-8fbf-1cc9070042d2";
-    # };
-    # sudo iscsiadm -m node -o new -T  -p
-    # sudo iscsiadm -m node -o update -T iqn.2015-12.com.oracleiaas:6a2e8816-5763-4768-8fbf-1cc9070042d2 -n node.startup -v automatic
   };
 }
