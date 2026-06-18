90405ea17e63e79e5ad749b38e5c34b190a7936c
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-18 14:32:21 +0200

    hosts/poopy: use serial console

--- hosts/poopy/hardware-configuration.nix
+++ hosts/poopy/hardware-configuration.nix
@@ -1,17 +1,30 @@
-{ modulesPath, ... }:
-{
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.loader = {
-    efi.efiSysMountPoint = "/boot/efi";
-    grub = {
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-      device = "nodev";
+{modulesPath, ...}: {
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+  boot = {
+    loader = {
+      efi.efiSysMountPoint = "/boot/efi";
+      grub = {
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+        device = "nodev";
+        extraConfig = ''
+          serial --unit=0 --speed=115200
+          terminal_input serial console
+          terminal_output serial console
+        '';
+      };
     };
+
+    initrd = {
+      availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+      kernelModules = ["nvme"];
+    };
+
+    kernelParams = ["console=tty0" "console=ttyS0,115200"];
   };
-  fileSystems."/boot/efi" = { device = "/dev/disk/by-uuid/C345-7B38"; fsType = "vfat"; };
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
-  boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
-  
+
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
 }
