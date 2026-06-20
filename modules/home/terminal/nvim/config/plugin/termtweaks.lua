2d040b4d4063b0f25ffec6522cbdf0a9be5a2f23
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-21 01:56:23 +0200

    nvim: tweaks

--- modules/home/terminal/nvim/config/plugin/termtweaks.lua
+++ modules/home/terminal/nvim/config/plugin/termtweaks.lua
@@ -32,3 +32,23 @@ vim.api.nvim_create_autocmd({ "BufLeave" }, {
 		vim.cmd.checktime()
 	end,
 })
+
+vim.api.nvim_create_autocmd('TermRequest', {
+	group = augroup,
+	desc = 'Handles OSC 7 dir change requests',
+	callback = function(ev)
+		local val, n = string.gsub(ev.data.sequence, '\027]7;file://[^/]*', '')
+		if n > 0 then
+			-- OSC 7: dir-change
+			local dir = val
+			if vim.fn.isdirectory(dir) == 0 then
+				vim.notify('invalid dir: ' .. dir)
+				return
+			end
+			vim.b[ev.buf].osc7_dir = dir
+			if vim.api.nvim_get_current_buf() == ev.buf then
+				vim.cmd.lcd(dir)
+			end
+		end
+	end,
+})
