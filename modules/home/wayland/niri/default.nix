c8ba11ad703e964403ade05924b7fdbc3542ba7e
Ching Pei Yang <badnam3o.0@gmail.com>
2026-06-26 12:44:43 +0200

    niri: add config

--- modules/home/wayland/niri/default.nix
+++ modules/home/wayland/niri/default.nix
@@ -1,4 +1,6 @@
 {impurity, ...}: {
   xdg.configFile."xdg-desktop-portal/niri-portals.conf".source =
     impurity.link ./niri-portals.conf;
+  xdg.configFile."niri/config.kdl".source =
+    impurity.link ./config.kdl;
 }
