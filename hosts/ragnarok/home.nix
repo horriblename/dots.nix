46a2fc3c1aa00c41a71a4478892a0c941be325d7
Ching Pei Yang <badnam3o.0@gmail.com>
2026-07-20 21:20:41 +0200

    hosts/ragnarok: update

--- hosts/ragnarok/home.nix
+++ hosts/ragnarok/home.nix
@@ -1,20 +1,5 @@
-{
-  lib,
-  pins,
-  ...
-}: let
+{lib, ...}: let
   inherit (lib.modules) mkForce;
-
-  noBuildPlug = pname: let
-    pin = pins.${pname};
-    version = builtins.substring 0 8 pin.revision;
-  in
-    pin.outPath.overrideAttrs {
-      inherit pname version;
-      name = "${pname}-${version}";
-
-      passthru.vimPlugin = false;
-    };
 in {
   imports = [
     ./llama.nix
@@ -27,12 +12,9 @@ in {
       haskell.enable = mkForce true;
       html.enable = mkForce true;
       lua.enable = mkForce true;
-      tex.enable = mkForce true;
       rust.enable = mkForce true;
-    };
-
-    extraPlugins = {
-      llama = {package = noBuildPlug "llama.vim";};
+      typst.enable = mkForce false;
+      zig.enable = mkForce true;
     };
   };
 }
